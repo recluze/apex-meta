b43f4046df5ca4a5cb94eb5f139db86575cc3a3f
Nauman <recluze@gmail.com>
2013-07-14 12:35:08 +0500

    Reduced size of screenshot taken device art

--- screenshot-taker/crop-emu.sh
+++ screenshot-taker/crop-emu.sh
@@ -1,6 +1,6 @@
 # to create a black image 
 # convert -size 340x500 xc:#000000 black.png 
 convert $1 -crop 331x491+23+21 out-$1
-convert  skin-1.png black.png -compose over -geometry '500x775+135+245!' -composite out-$1 -compose over  -geometry "550x720+140+265" -composite res.png
+convert  skin-2.png black.png -compose over -geometry '500x700+125+240!' -composite out-$1 -compose over  -geometry "475x680+150+250" -composite final-$1
 
 
